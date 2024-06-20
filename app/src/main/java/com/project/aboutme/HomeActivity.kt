197a6b9745f937042dd4fd75b0196c1b5a393798
Rure00 <asnjkd5193@gmail.com>
2024-06-20 13:16:49 +0900

    get Id from extras In HomeActivity.

--- app/src/main/java/com/project/aboutme/HomeActivity.kt
+++ app/src/main/java/com/project/aboutme/HomeActivity.kt
@@ -1,12 +1,18 @@
 package com.project.aboutme
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+    private lateinit var idText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,11 @@ class HomeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        imageView = findViewById(R.id.image)
+        idText = findViewById(R.id.user_id)
+
+        val id = intent.getStringExtra("id") ?: throw Exception("Id is Not Found...")
+        idText.text = id
     }
 }
